43d4fed774cbcb11605ccc4e0ff802c7471dace6
RobinRheem <roborheems@gmail.com>
2019-06-24 17:57:19 +0900

    Configure Not Null and precision and scale for ThemeLight

--- src/main/java/com/cladup/hyperion/themelight/ThemeLight.java
+++ src/main/java/com/cladup/hyperion/themelight/ThemeLight.java
@@ -7,10 +7,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -24,13 +21,26 @@ public class ThemeLight {
     @Id
     @GeneratedValue
     private long id;
+    @Column(nullable = false)
     private String type;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private boolean castShadow;
+    @Column(nullable = false,
+            precision = 5, scale = 3)
     private BigDecimal angle;
+    @Column(nullable = false,
+            precision = 5, scale = 3)
     private String color;
+    @Column(nullable = false,
+            precision = 5, scale = 3)
     private BigDecimal distance;
+    @Column(nullable = false,
+            precision = 5, scale = 3)
     private BigDecimal intensity;
+    @Column(nullable = false,
+            precision = 5, scale = 3)
     private BigDecimal spotPenumbra;
     @UpdateTimestamp
     private LocalDateTime updatedAt;
