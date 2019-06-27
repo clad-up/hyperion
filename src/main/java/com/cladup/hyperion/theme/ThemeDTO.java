4027205a94bfa1904131e0ad012b8053cc59ed4d
RobinRheem <roborheems@gmail.com>
2019-06-27 16:49:16 +0900

    Add null related anotations to ThemeVO and ThemeDTO

--- src/main/java/com/cladup/hyperion/theme/ThemeDTO.java
+++ src/main/java/com/cladup/hyperion/theme/ThemeDTO.java
@@ -5,6 +5,8 @@ import com.cladup.hyperion.themeobject.ThemeObjectDTO;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -12,24 +14,43 @@ import java.util.List;
 @Builder(toBuilder = true)
 public class ThemeDTO {
     private final long id;
+    @NotBlank
     private final String name;
+    @NotNull
     private final String description;
+    @NotNull
     private final BigDecimal cameraPositionX;
+    @NotNull
     private final BigDecimal cameraPositionY;
+    @NotNull
     private final BigDecimal cameraPositionZ;
+    @NotNull
     private final BigDecimal cameraRotationX;
+    @NotNull
     private final BigDecimal cameraRotationY;
+    @NotNull
     private final BigDecimal cameraRotationZ;
+    @NotNull
     private final BigDecimal positionX;
+    @NotNull
     private final BigDecimal positionY;
+    @NotNull
     private final BigDecimal positionZ;
+    @NotNull
     private final BigDecimal rotationX;
+    @NotNull
     private final BigDecimal rotationY;
+    @NotNull
     private final BigDecimal rotationZ;
+    @NotNull
     private final BigDecimal scaleX;
+    @NotNull
     private final BigDecimal scaleY;
+    @NotNull
     private final BigDecimal scaleZ;
+    @NotNull
     private final List<ThemeObjectDTO> themeObjects;
+    @NotNull
     private final List<ThemeLightDTO> themeLights;
 
 }
--- src/main/java/com/cladup/hyperion/theme/ThemeVO.java
+++ src/main/java/com/cladup/hyperion/theme/ThemeVO.java
@@ -2,34 +2,57 @@ package com.cladup.hyperion.theme;
 
 import com.cladup.hyperion.themelight.ThemeLightVO;
 import com.cladup.hyperion.themeobject.ThemeObjectVO;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 @Builder(toBuilder = true)
 public class ThemeVO {
     private final long id;
+    @NotBlank
     private final String name;
+    @NotNull
     private final String description;
+    @NotNull
     private final BigDecimal cameraPositionX;
+    @NotNull
     private final BigDecimal cameraPositionY;
+    @NotNull
     private final BigDecimal cameraPositionZ;
+    @NotNull
     private final BigDecimal cameraRotationX;
+    @NotNull
     private final BigDecimal cameraRotationY;
+    @NotNull
     private final BigDecimal cameraRotationZ;
+    @NotNull
     private final BigDecimal positionX;
+    @NotNull
     private final BigDecimal positionY;
+    @NotNull
     private final BigDecimal positionZ;
+    @NotNull
     private final BigDecimal rotationX;
+    @NotNull
     private final BigDecimal rotationY;
+    @NotNull
     private final BigDecimal rotationZ;
+    @NotNull
     private final BigDecimal scaleX;
+    @NotNull
     private final BigDecimal scaleY;
+    @NotNull
     private final BigDecimal scaleZ;
+    @NotNull
     private final List<ThemeObjectVO> themeObjects;
+    @NotNull
     private final List<ThemeLightVO> themeLights;
 
 }
