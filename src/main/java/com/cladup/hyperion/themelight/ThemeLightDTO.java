5267fe42258807959cd3facf4f538271d7d76193
RobinRheem <roborheems@gmail.com>
2019-06-27 16:52:08 +0900

    Add null related annotations for ThemeLightVO and ThemeLightDTO

--- src/main/java/com/cladup/hyperion/themelight/ThemeLightDTO.java
+++ src/main/java/com/cladup/hyperion/themelight/ThemeLightDTO.java
@@ -3,19 +3,27 @@ package com.cladup.hyperion.themelight;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
 @Builder(toBuilder = true)
 public class ThemeLightDTO {
     private final long id;
+    @NotBlank
     private final String type;
+    @NotBlank
     private final String name;
     private final boolean castShadow;
     private final int angle;
+    @NotBlank
     private final String color;
+    @NotNull
     private final BigDecimal distance;
+    @NotNull
     private final BigDecimal intensity;
+    @NotNull
     private final BigDecimal spotPenumbra;
 
 }
