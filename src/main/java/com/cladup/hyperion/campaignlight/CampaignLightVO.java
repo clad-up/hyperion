129ba1e1b726f81d7c2364e060a931a5d1beae53
RobinRheem <roborheems@gmail.com>
2019-07-01 11:45:21 +0900

    Create value objects for campaign domain

---
+++ src/main/java/com/cladup/hyperion/campaignlight/CampaignLightVO.java
@@ -0,0 +1,29 @@
+package com.cladup.hyperion.campaignlight;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Data
+@Builder(toBuilder = true)
+public class CampaignLightVO {
+    private final long id;
+    @NotBlank
+    private final String type;
+    @NotBlank
+    private final String name;
+    private final boolean castShadow;
+    private final int angle;
+    @NotBlank
+    private final String color;
+    @NotNull
+    private final BigDecimal distance;
+    @NotNull
+    private final BigDecimal intensity;
+    @NotNull
+    private final BigDecimal spotPenumbra;
+
+}
---
+++ src/main/java/com/cladup/hyperion/campaignobject/CampaignObjectVO.java
@@ -0,0 +1,45 @@
+package com.cladup.hyperion.campaignobject;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Data
+@Builder(toBuilder = true)
+public class CampaignObjectVO {
+    private final long id;
+    @NotBlank
+    private final String name;
+    @NotBlank
+    private final String type;
+    @NotNull
+    private final BigDecimal positionX;
+    @NotNull
+    private final BigDecimal positionY;
+    @NotNull
+    private final BigDecimal positionZ;
+    @NotNull
+    private final BigDecimal rotationX;
+    @NotNull
+    private final BigDecimal rotationY;
+    @NotNull
+    private final BigDecimal rotationZ;
+    @NotNull
+    private final BigDecimal scaleX;
+    @NotNull
+    private final BigDecimal scaleY;
+    @NotNull
+    private final BigDecimal scaleZ;
+    @NotNull
+    private final String companyProductId;
+    @NotNull
+    private final String companyProductName;
+    @NotNull
+    private final String companyProductImageUrl;
+    @NotNull
+    private final String companyProductTargetUrl;
+
+}
